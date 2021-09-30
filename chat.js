fa10e389bafa0dd02490fcbc32a82b6e1de3d48b
machete325 <machete325@gmail.com>
2021-10-01 02:45:37 +0300

    Ok

--- chat.js
+++ chat.js
@@ -17,6 +17,12 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 io.on('connection', (socket) => {
   socket.on('message', ({ name, message }) => {
     io.emit('message', { name, message });
